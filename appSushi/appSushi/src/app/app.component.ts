4e50f7e07566cfc81b97a286ece7a8e1046f45ab
Moustik <dorianmx77@gmail.com>
2026-01-16 10:45:25 +0100

    pomm

--- appSushi/appSushi/src/app/app.component.ts
+++ appSushi/appSushi/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RegisterComponent} from './auth/register/register.component';
-
+import { AccueilComponent } from './accueil/accueil.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RegisterComponent, ],
+  imports: [CommonModule, RegisterComponent, AccueilComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
